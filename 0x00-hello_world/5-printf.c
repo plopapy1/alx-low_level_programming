8011c19bf21dd0a35510c3f0515996980a88b448
plopapy1 <plopapy@gmail.com>
2023-03-16 14:46:03 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+*main - the main entry point
+*return : always 0 (sucess)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
