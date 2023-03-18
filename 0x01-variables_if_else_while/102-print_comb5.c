45dcdb4cdbccc56bcfe53e089d8d7fc39698c56e
plopapy1 <plopapy@gmail.com>
2023-03-17 20:22:16 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,18 +9,20 @@ int main(void)
 	int q;
 
 	for (p = 0; p <= 98; p++)
-
+	{
 	for (q = 0; q <= 99; q++)
-
+	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
 	putchar(' ');
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
 	if (p == 98 && q == 99)
+	continue;
 	putchar(',');
 	putchar(' ');
-
+	}
+	}
 	putchar('\n');
 	return (0);
 }
