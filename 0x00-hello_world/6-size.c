fa81c55a4cf78659fcaf0a6add9f6d4837551d37
plopapy1 <plopapy@gmail.com>
2023-03-20 04:13:54 -0700

    print 32bit correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,15 @@
 */
 int main(void)
 {
-	printf("size of a char: %i byte(s)\n", sizeof(char));
-	printf("size of a int: %i byte(s)\n", sizeof(int));
-	printf("size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("size of a float: %i byte(s)", sizeof(float));
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of a int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
