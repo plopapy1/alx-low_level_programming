838e41b5c8cdf8212c86eeb73d7e5a992d763346
plopapy1 <plopapy@gmail.com>
2023-03-21 13:41:31 -0700

    alphabet print

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
-
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
+/**
+  * print_alphabet - printing my alphabet
+  * Return: 0
+  */
 
 	void print_alphabet(void)
 	{
