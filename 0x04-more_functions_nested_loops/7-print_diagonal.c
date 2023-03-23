ebcddc9e31dd1acb4ffeef09118e2f7f7e48a5b4
plopapy1 <plopapy@gmail.com>
2023-03-23 15:10:33 -0700

    7-print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,18 +11,19 @@ void print_diagonal(int n)
 
 	if  (n <= 0)
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
-	for (x = 0; x < n; x++)
-	}
-	for (y = 0; y < x; y++)
-	{
-	 _putchar(32);
+		for (x = 0; x < n; x++)
+
+		for (y = 0; y < x; y++)
+
+		{
+		_putchar(32);
+		}
 	}
 	_putchar(92);
 	_putchar('\n');
-	}
-	}
+
 }
