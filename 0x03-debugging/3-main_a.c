374ea75f341f820a51ffe0c29f8a05b0518a535c
plopapy1 <plopapy@gmail.com>
2023-03-23 10:58:22 -0700

    correction to the last task

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
 * main - takes a date and prints how many days are left in the year, taking
 * leap years into account
 * Return: 0
 */
-
 int main(void)
 {
 	int month;
 	int day;
 	int year;
-
 	month = 2;
 	day = 29;
 	year = 2000;
