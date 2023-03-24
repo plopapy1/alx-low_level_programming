168fc323f73a4e9d575dcbd21317a0fda6694ce4
plopapy1 <plopapy@gmail.com>
2023-03-24 13:10:39 -0700

    diagonal printing corrected

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,22 +7,26 @@
 
 void print_diagonal(int n)
 {
-	int x, y;
 
 	if  (n <= 0)
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
-	for (x = 0; x <= n; x++)
-	{
-	for (y = 0; y < x; y++)
-	{
-	_putchar(32);
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
 	}
-	_putchar(92);
-	_putchar('\n');
-	}
-	}
+
 }
