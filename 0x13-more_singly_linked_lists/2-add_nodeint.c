e6a9ed5b4bb407c619ca12bd709a7d16163fa5f4
plopapy1 <plopapy@gmail.com>
2023-05-01 06:44:53 -0700

    add

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,15 +18,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	/*link first node in head with the new_node*/
 	if (*head == NULL)
 		new_node->next = NULL;
 	else
 		new_node->next = *head;
 
-	new_node->s = s;
-	/*add new node at the beginning of the list*/
-	*head = new_node;
+	new_node->n = n;
 
+	*head = new_node;
 	return (*head);
 }
