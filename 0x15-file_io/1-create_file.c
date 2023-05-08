26e4055ba7c3e8f8b1afb912dcb72ceb6f60c733
plopapy1 <plopapy@gmail.com>
2023-05-08 11:52:40 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,69 +1,33 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include "main.h"
 
 /**
- * create_file - append to file
- * @filename: path to file
- * @text_content: content
- * Return: chars read
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t w;
-	int size;
-	char *mem;
+	int fd, w, len = 0;
 
-	if (!filename)
-	{
+	if (filename == NULL)
 		return (-1);
-	}
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-	if (!text_content)
+
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	size = _strlen(text_content);
-	mem = malloc(sizeof(char) * size);
-	if (!mem)
-	{
-		close(fd);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
-	}
-	w = write(fd, text_content, size);
-	if (w == -1)
-	{
-		close(fd);
-		free(mem);
-		return (-1);
-	}
+
 	close(fd);
-	free(mem);
+
 	return (1);
-}
-
-/**
- * _strlen - len
- *
- * @s: is a pointer to a char
- *
- * Return: Always 0.
- */
-
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-
-	return (i);
 }
